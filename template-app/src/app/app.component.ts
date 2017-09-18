980b47a03c94c50a71a680e43ce70461d0cd52e6
Shane <shaneroode@gmail.com>
2017-09-18 19:57:18 +1000

    Play with the layout

--- template-app/src/app/app.component.ts
+++ template-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  navOpen = true;
+
+  toggleNav(): void {
+    console.log("toggleNav: ", this.navOpen);
+  }
 }
