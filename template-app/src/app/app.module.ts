a282ae84f489e5ecbcb386f0ede4f622fbd974ed
Shane <shaneroode@gmail.com>
2017-09-19 20:18:20 +1000

    Added MainComponent

--- template-app/src/app/app.module.ts
+++ template-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
