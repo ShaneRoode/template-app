6013929641fc52355fdb73d11c6f22a9e73e22a1
Shane <shaneroode@gmail.com>
2017-09-19 20:16:41 +1000

    Added Menucomponent

--- template-app/src/app/app.module.ts
+++ template-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
