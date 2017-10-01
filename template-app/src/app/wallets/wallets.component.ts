17d000554c0bb6f704138dcfcb4ca2901d0d6016
Shane <shaneroode@gmail.com>
2017-10-01 12:00:06 +1100

    Added Wallet form with list

--- template-app/src/app/wallets/wallets.component.ts
+++ template-app/src/app/wallets/wallets.component.ts
@@ -10,9 +10,52 @@ export class WalletsComponent implements OnInit {
 
   title = 'Wallets Component';
 
+  wallet: Wallet = new Wallet;
+  wallets: Wallet[];
+
   constructor() { }
 
   ngOnInit() {
+    this.wallets = this.getWallets();
   }
 
+  getWallets(): Wallet[] {
+
+    const wallet1 = new Wallet;
+    wallet1.id = 1;
+    wallet1.name = 'BitCoin';
+    wallet1.units = 0.5031221;
+
+    const wallet2 = new Wallet;
+    wallet2.id = 2;
+    wallet2.name = 'Neo';
+    wallet2.units = 285.5031221;
+
+    return [
+      wallet1,
+      wallet2
+    ];
+  }
+
+  addWallet() {
+
+    const wallet = new Wallet();
+    wallet.name = this.wallet.name;
+    wallet.units = this.wallet.units;
+    console.log(wallet);
+    this.wallets.push(wallet);
+    this.wallet = new Wallet;
+  }
+
+}
+
+class Wallet {
+
+  id: number;
+  name: string;
+  units: number;
+
+  constructor() {
+
+  }
 }
