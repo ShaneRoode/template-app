861695bb0a843a3a9cf9dcba489e6128d889c7a1
Shane <shaneroode@gmail.com>
2017-10-01 12:41:37 +1100

    finish adding in trade feature

--- template-app/src/app/trades/trades.component.ts
+++ template-app/src/app/trades/trades.component.ts
@@ -10,17 +10,30 @@ export class TradesComponent implements OnInit {
 
   title = 'Trades Component';
   trades: Trade[];
+  trade: Trade = new Trade;
 
-  constructor() { 
+  constructor() {
   }
 
   ngOnInit() {
-
-
     this.trades = this.getTrades();
   }
 
-  getTrades(): Trade[]{
+  addTrade() {
+    const trade = new Trade;
+    trade.type = this.trade.type;
+    trade.openedDate = new Date();
+    trade.closedDate = new Date();
+    trade.bidAsk = this.trade.bidAsk;
+    trade.unitsFilled = this.trade.unitsFilled;
+    trade.unitsTotal = this.trade.unitsTotal;
+    trade.actualRate = this.trade.actualRate;
+    trade.costProceeds = this.trade.costProceeds;
+
+    this.trades.push(trade);
+  }
+
+  getTrades(): Trade[] {
 
     const trade1 = new Trade();
     trade1.type = TradeType.LimitBuy;
@@ -29,8 +42,8 @@ export class TradesComponent implements OnInit {
     trade1.bidAsk = 0.00514799;
     trade1.unitsFilled = 23.55976148;
     trade1.unitsTotal = 23.55976148;
-    trade1.acutalRate = 0.00514004;
-    trade1.costOverProceeds = -0.12140109;
+    trade1.actualRate = 0.00514004;
+    trade1.costProceeds = -0.12140109;
 
     const trade2 = new Trade();
     trade2.type = TradeType.LimitBuy;
@@ -39,8 +52,8 @@ export class TradesComponent implements OnInit {
     trade2.bidAsk = 0.00514799;
     trade2.unitsFilled = 23.55976148;
     trade2.unitsTotal = 23.55976148;
-    trade2.acutalRate = 0.00514004;
-    trade2.costOverProceeds = -0.12140109;
+    trade2.actualRate = 0.00514004;
+    trade2.costProceeds = -0.12140109;
 
     return [
       trade1,
@@ -58,12 +71,10 @@ class Trade {
   bidAsk: number;
   unitsFilled: number;
   unitsTotal: number;
-  acutalRate: number;
-  costOverProceeds: number;
+  actualRate: number;
+  costProceeds: number;
 
-  constructor(){
-
-  }
+  constructor() { }
 }
 
 enum TradeType {
